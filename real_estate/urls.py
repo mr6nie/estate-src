d876fe7b1f148dbf33aec53de337b15dca1fb8e4
ebasosinka1 <mr.6nie@gmail.com>
2022-01-07 14:04:08 +0400

    feat: user auth register, login, password reset

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
+from django.urls.conf import include
 
 urlpatterns = [
     path("supersecret/", admin.site.urls),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "Real Estate Admin"
